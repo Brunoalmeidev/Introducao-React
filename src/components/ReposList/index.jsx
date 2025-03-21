c7ccae5590b011a540a0db7aa84c74b66054b82e
Bruno Almeida <brunoalmeida2846@hotmail.com>
2025-03-21 17:43:06 -0300

    refactor: melhorar requisição de repositórios no ReposList

--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -1,15 +1,26 @@
 import { useEffect, useState } from "react";
-
 import styles from './ReposList.module.css';
 
 const ReposList = ({ nomeUsuario }) => {
     const [repos, setRepos] = useState([]);
     const [estaCarregando, setEstaCarregando] = useState(true);
+    const [erro, setErro] = useState(null);
 
     useEffect(() => {
         setEstaCarregando(true);
+        setErro(null);
+
         fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
-            .then(res => res.json())
+            .then(res => {
+                if (!res.ok) {
+                    if (res.status === 404) {
+                        throw new Error("Usuário não encontrado.");
+                    } else {
+                        throw new Error("Erro ao buscar os repositórios.");
+                    }
+                }
+                return res.json();
+            })
             .then(resJson => {
                 setTimeout(() => {
                     setEstaCarregando(false);
@@ -17,15 +28,20 @@ const ReposList = ({ nomeUsuario }) => {
                 }, 2000);
 
             })
+            .catch(e => {
+                setEstaCarregando(false);
+                setErro(e.message);
+            })
     }, [nomeUsuario]);
 
     return (
         <div className="container">
             {estaCarregando ? (
                 <h1>Carregando...</h1>
+            ) : erro ? (
+                <h1>{erro}</h1>
             ) : (
                 <ul className={styles.list}>
-                    {/* {repos.map(repositorio => ( */}
                     {repos.map(({ id, name, language, html_url }) => (
                         <li className={styles.listItem} key={id}>
                             <div className={styles.itemName}>
